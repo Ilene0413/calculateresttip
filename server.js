397bedd9b140e6a5c28509e61a81e99f00eb30b9
Ilene Cohen <ilene413@icloud.com>
2019-06-06 11:51:17 -0400

    ask for user input-begin validation

---
+++ server.js
@@ -0,0 +1,28 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("app/public"));
+
+// ROUTER
+// The below points our server to a series of "route" files.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// Start the API server
+app.listen(PORT, function() {
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
